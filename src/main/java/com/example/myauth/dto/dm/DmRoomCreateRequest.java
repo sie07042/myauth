65ab89e0e87b008085f7236a1291c145297ce8b0
sie07042 <sie07042@naver.com>
2026-03-14 19:04:54 +0900

    test

--- src/main/java/com/example/myauth/dto/dm/DmRoomCreateRequest.java
+++ src/main/java/com/example/myauth/dto/dm/DmRoomCreateRequest.java
@@ -1,5 +1,6 @@
 package com.example.myauth.dto.dm;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,5 +10,6 @@ import lombok.NoArgsConstructor;
 public class DmRoomCreateRequest {
 
     @NotNull(message = "대상 사용자 ID는 필수입니다.")
+    @JsonAlias("peerUserId")
     private Long targetUserId;
 }
